fe7f36557b12f6920e543e4f3264018b748495cf
tranvancu185 <84246142+tranvancu185@users.noreply.github.com>
2021-12-12 20:58:10 +0700

    #5 design admin screen

---
+++ WebKaraoke/WebKaraoke/wwwroot/js/main.js
@@ -0,0 +1,8 @@
+﻿function showPwd() {
+    var x = document.getElementById("pwdInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
